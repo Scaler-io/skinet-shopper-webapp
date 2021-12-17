012bf0aedd5c21ec93d9eb89ac7baa4d3ccd0a1d
sayan95 <sayanmallik1995@gmail.com>
2021-12-17 21:27:08 +0530

    Products are fetched from api. basic ui template is ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './ui/nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavBarComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent, NavBarComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/models/productMetaData.ts
@@ -0,0 +1,6 @@
+export interface ProductMetaData{
+    createdBy: string;
+    createdAt: Date;
+    lastModifiedAt: Date;
+    lastModifieddBy: string;
+}
