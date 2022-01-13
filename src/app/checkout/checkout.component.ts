849d7cab8c383b1dd0529f4384ee0ef413bba838
sayan95 <sayanmallik1995@gmail.com>
2022-01-13 22:09:48 +0530

    checkout base layout created

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-checkout',
+  selector: 'skinet-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
