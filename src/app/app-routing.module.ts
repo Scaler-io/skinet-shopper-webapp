b9dce65a3ec61fa73d7598672a8abb443372e0d4
sayan95 <sharthakmallik@gmail.com>
2022-01-16 23:10:42 +0530

    Orders listing feature initial setup done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,9 +21,13 @@ const routes: Routes = [
   {path: 'checkout', canActivate: [AuthGuard, CheckoutGuard], loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule),
     data: {breadcrumb: 'Checkout'}
   },
+  {path: 'orders', loadChildren: () => import('./orders/orders.module').then(mod => mod.OrdersModule),
+    data: {breadcrumb: 'Orders'}
+  },
   {path: 'account', loadChildren: () => import('./account/account.module').then(mod => mod.AccountModule),
     data: {breadcrumb: {skip: true}}
   },
+
   {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
 ];
 
